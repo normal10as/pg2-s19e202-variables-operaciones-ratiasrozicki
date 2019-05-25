3399a41d89b4a3790b63a037f5693d65e1aea8ff
ratiasrozicki <49293440+ratiasrozicki@users.noreply.github.com>
2019-05-25 00:08:20 -0300

    variables

--- 1.13.Mayor/Program.vb
+++ 1.13.Mayor/Program.vb
@@ -1,25 +1,17 @@
-Imports System
+Imports System.Math
+
 
 Module Mayor
-    Dim numero1, numero2, numero3 As Integer
-    Dim mayor
+    Dim numero1, numero2, numero3, mayor1, mayor2 As Integer
     Sub Main(args As String())
-        mayor = numero1
         Console.Write("Ingrese el Primer numero: ")
         numero1 = Console.ReadLine()
         Console.Write("Ingrese el Segundo numero: ")
         numero2 = Console.ReadLine()
         Console.Write("Ingrese el Tercer numero: ")
         numero3 = Console.ReadLine()
-        If numero1 >= numero2 And numero1 >= numero3 Then
-            mayor = numero1
-        End If
-        If numero2 >= numero1 And numero2 >= numero3 Then
-            mayor = numero2
-        End If
-        If numero3 >= numero2 And numero3 >= numero1 Then
-            mayor = numero3
-        End If
-        Console.WriteLine("El mayor es: " & mayor)
+        mayor1 = Max(numero1, numero2)
+        mayor2 = Max(mayor1, numero3)
+        Console.WriteLine("El mayor es: " & mayor2)
     End Sub
 End Module
